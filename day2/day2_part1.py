a34694b32870c6f805520313b81bee28c2147642
Alex Cohen <cohenah@clarkson.edu>
2022-12-01 01:21:07 -0500

    ready for day 2

---
+++ day2/day2_part1.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("aocd > inputs/input-2022-2.txt")
+
+file = open("inputs/input-2022-2.txt")
+
+for line in file:
+    print(line)
---
+++ day2/day2_part2.py
@@ -0,0 +1,4 @@
+file = open("inputs/input-2022-2.txt")
+
+for line in file:
+    print(line)
