b6e131cdc6d8956294c7babdc6bd249407c5596a
Alex Cohen <cohenah@clarkson.edu>
2022-11-30 21:16:49 -0500

    ready to start

--- day 1/part1.py
+++ day 1/part1.py
@@ -1,8 +1,5 @@
-import os
-
-os.system("aocd > inputs/input-2022-1.txt")
-
-file = open("inputs/input-2022-1.txt")
+# file = open("inputs/input-2022-1.txt")
+file = open("test.txt")
 
 for line in file:
     print(line)
--- day 1/part2.py
+++ day 1/part2.py
@@ -1,4 +1,5 @@
-file = open("inputs/input-2022-1.txt")
+# file = open("inputs/input-2022-1.txt")
+file = open("test.txt")
 
 for line in file:
     print(line)
