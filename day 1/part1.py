a3bac264bd54193500af482a51de409c6291643b
Alex Cohen <cohenah@clarkson.edu>
2022-12-01 00:18:43 -0500

    day 1 complete

--- day 1/part1.py
+++ day 1/part1.py
@@ -1,5 +1,23 @@
-# file = open("inputs/input-2022-1.txt")
-file = open("test.txt")
+file = open("inputs/input-2022-1.txt")
+# file = open("test.txt")
 
+cal = []
+buff = []
 for line in file:
-    print(line)
+    if line != '\n':
+        buff.append(int(line.strip()))
+    else:
+        cal.append(buff)
+        buff = []
+cal.append(buff)
+
+max = 0
+for i in cal:
+    sum = 0
+    for j in i:
+        sum = sum + j
+    if sum > max:
+        max = sum
+
+# answer
+print(max)
