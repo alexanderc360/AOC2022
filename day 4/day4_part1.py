299fa34cfd8470d572c3e35bf9985e0b5a97ff7c
Alexander Cohen <cohenah@clarkson.edu>
2022-12-04 00:22:15 -0500

    day 4 done

--- day 4/day4_part1.py
+++ day 4/day4_part1.py
@@ -1,5 +1,28 @@
-# file = open("inputs/input-2022-4.txt")
-file = open("test.txt")
+file = open("inputs/input-2022-4.txt")
 
+list = []
 for line in file:
-    print(line)
+    buff = line.strip().split(',')
+    pair = []
+    for i in buff:
+        pair.append(i.split('-'))
+    list.append(pair)
+
+print(list[0])
+count = 0
+for i in list:
+    f = i[0]
+    d = i[1]
+    fbuff = []
+    dbuff = []
+    for j in range(int(f[0]), int(f[1])+1, 1):
+        fbuff.append(j)
+    for j in range(int(d[0]), int(d[1])+1, 1):
+        dbuff.append(j)
+    
+    if fbuff[0] in dbuff and fbuff[-1] in dbuff:
+        count +=1
+    elif dbuff[0] in fbuff and dbuff[-1] in fbuff:
+        count +=1
+
+print(count)
