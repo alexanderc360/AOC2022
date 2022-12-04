1b634d6cd312be1dba0c221d5aee3a6fe92ad18a
Alexander Cohen <cohenah@clarkson.edu>
2022-12-04 00:53:43 -0500

    used regex on input parse, replaced for loop with range

--- day 4/day4_part1.py
+++ day 4/day4_part1.py
@@ -1,28 +1,20 @@
+import re
+
 file = open("inputs/input-2022-4.txt")
 
-list = []
+pairs = []
 for line in file:
-    buff = line.strip().split(',')
-    pair = []
-    for i in buff:
-        pair.append(i.split('-'))
-    list.append(pair)
+    buff = re.split(',|-', line.strip())
+    pairs.append([buff[0], buff[1], buff[2], buff[3]])
 
-print(list[0])
 count = 0
-for i in list:
-    f = i[0]
-    d = i[1]
-    fbuff = []
-    dbuff = []
-    for j in range(int(f[0]), int(f[1])+1, 1):
-        fbuff.append(j)
-    for j in range(int(d[0]), int(d[1])+1, 1):
-        dbuff.append(j)
-    
+for i in pairs:
+    fbuff = range(int(i[0]), int(i[1])+1, 1)
+    dbuff = range(int(i[2]), int(i[3])+1, 1)
+
     if fbuff[0] in dbuff and fbuff[-1] in dbuff:
-        count +=1
+        count += 1
     elif dbuff[0] in fbuff and dbuff[-1] in fbuff:
-        count +=1
+        count += 1
 
 print(count)
