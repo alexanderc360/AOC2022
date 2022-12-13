09fce86f0b5e079ba2069ea1bdf77989a7abd019
Alex Cohen <cohenah@clarkson.edu>
2022-12-13 04:37:49 -0500

    works on test input, ugh

--- day 13/day13_part1.py
+++ day 13/day13_part1.py
@@ -2,19 +2,62 @@ import os
 
 os.system("aocd > inputs/input-2022-13.txt")
 
-# file = open("inputs/input-2022-13.txt")
-file = open("test.txt")
+file = open("inputs/input-2022-13.txt")
+# file = open("test.txt")
 
 
-def compare(first, second):
-    print(type(first), type(second))
+def compareLists(one, two):  # returns if one comes before two
+    while len(one) > 0:
+        if len(two) == 0:
+            return False
 
-    while len(first) > 0 and len(second) > 0:
-        f = first.pop()
-        s = second.pop()
-        print(f, s)
+        if one[0] < two[0]:
+            return True
+        elif one[0] > two[0]:
+            return False
+        else:
+            one.pop()
+            two.pop()
+    if len(two) > 0:
+        return True
 
-    print()
+
+def foo(first, second):
+    if len(first) > 0:
+        if len(second) == 0:
+            return False
+        f = first[0]
+        s = second[0]
+        # print(first, second)
+        if type(f) == int:
+            # print("int")
+            if type(s) == int:
+                # print('im int too')
+                if f < s:
+                    return True
+                elif f > s:
+                    return False
+                else:
+                    return foo(first[1:], second[1:])
+            elif type(s) == list:
+                # print('oops')
+                first[0] = [f]
+                return foo(first, second)
+        elif type(f) == list:
+            # print('list')
+            if type(s) == int:
+                # print('oops, but now im an int')
+                second[0] = [s]
+                return foo(first, second)
+            elif type(s) == list:
+                # print('im a list too')
+                # if foo(f, s) == None:
+                #     print('lists equal')
+                return foo(f, s)
+                # else:
+                #     return compareLists(f, s)
+    else:
+        return True
 
 
 pairs = []
@@ -24,17 +67,21 @@ for line in file:
         pairs.append(buff)
         buff = []
     else:
-        buff.append(line.strip())
+        # buff.append([i for i in line.strip()])
+        buff.append(eval(line))
 pairs.append(buff)
 
-# for i in pairs:
-#     compare(i[0], i[1])
 
-
-a = [1, 2, 3]
-b = [1, 2, 1]
-
-c = compare(a, b)
-print(c)
 # for i in pairs:
 # print(i)
+
+count = 0
+ind = 1
+for i in pairs:
+    # print(ind)
+    if foo(i[0], i[1]):
+        count += ind
+        # print("true", count)
+
+    ind += 1
+print(count)
